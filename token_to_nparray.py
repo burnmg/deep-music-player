239708d7b3bd762747a1acfdc3f5224f57b8f685
burnmg <rlin1992@hotmail.com>
2019-05-19 15:56:55 +0800

    update

--- token_to_nparray.py
+++ token_to_nparray.py
@@ -2,7 +2,7 @@ import numpy as np
 import pickle
 
 
-token_file = "music_tokens/chopin_tokens"
+token_file = "music_tokens/new_songs_game100"
 
 with open(token_file, "rb") as file:
     t = pickle.load(file)
@@ -13,16 +13,16 @@ res = -np.ones(np_dimension)
 for i in range(len(t)):
     for j in range(len(t[i])):
         print(t[i][j])
-        if t[i][j] != "<start>":
+        if t[i][j] != "start":
             temp = t[i][j].split("|")
 
             res[i][j] = list(map(lambda x: float(x), temp))
 
         else:
-            res[i][j] = [60.0,-1.0,-1.0,-1.0,-1.0,1.66]
+            res[i][j] = [-1,-1.0,-1.0,-1.0,-1.0,-1]
             print("ok")
 
-output_file = "music_tokens/output/chopin.npy"
+output_file = "music_tokens/output/new_songs_game100.npy"
 np.save(output_file, res)
 
 
